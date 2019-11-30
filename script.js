6065b0f04ec9b879b0e33760292e7bd4422faff9
Kevin Devine-Jones <kevin.jones215@gmail.com>
2019-11-30 13:41:34 -0500

    Style quiz card, declare functions, add questions

---
+++ script.js
@@ -0,0 +1,40 @@
+var questions = [
+    {
+        title: "Descendant of Isildur and rightful heir to the Thrones of Arnor and Gondor:",
+        choices: ["Aragorn", "Gandalf", "Frodo Baggins", "Galadriel"],
+        answer: "Aragorn"
+    },
+    {
+        title: "Cousin to Bilbo Baggins, inherits Bag End and undertakes the Quest to carry the One Ring to Mordor and destroy it in the fires of Mount Doom:",
+        choices: ["Frodo Baggins", "Denethor", "Saruman the White", "Treebeard"],
+        answer: "Frodo Baggins"
+    },
+    {
+        title: "A River Hobbit, obsessed with the One Ring who plots to steal it back:",
+        choices: ["Bilbo Baggins", "Samwise Gamgee", "Gollum", "Legolas"],
+        answer: "Gollum"
+    },
+    {
+        title: "A fallen Maia who helped the Elves of Eregion forge the Rings of Power in the Second Age:",
+        choices: ["Sauron", "Gandalf", "Saruman the White", "Shelob"],
+        answer: "Sauron"
+    },
+    {
+        title: "An Elf prince and son of King Thranduil of the Silvan Elves of Northern Mirkwood:",
+        choices: ["Frodo Baggins", "Gandalf", "Galadriel", "Legolas Greenleaf"],
+        answer: "Legolas Greenleaf"
+    },
+    {
+        title: "A Wizard, member of the Istari Order, and leader of the Fellowship of the Ring and The Army of the West:"
+        choices: ["Saruman the White", "Legolas Greenleaf", "Gollum", "Gandalf"],
+        answer: "Gandalf"
+    }
+];
+
+function startScreen(){}
+
+function startGame(){}
+
+function showHighScores(){}
+
+function nextQuestion(){}
