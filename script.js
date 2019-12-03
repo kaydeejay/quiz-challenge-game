5736521beb7620851745b803f7a367ed0eb90743
Kevin Devine-Jones <kevin.jones215@gmail.com>
2019-12-02 22:18:48 -0500

    start to build card/screen change function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var questions = [
     },
     {
         title: "A River Hobbit, obsessed with the One Ring who plots to steal it back:",
-        choices: ["Bilbo Baggins", "Samwise Gamgee", "Gollum", "Legolas"],
+        choices: ["Bilbo Baggins", "Samwise Gamgee", "Gollum", "Legolas Greenleaf"],
         answer: "Gollum"
     },
     {
@@ -25,16 +25,72 @@ var questions = [
         answer: "Legolas Greenleaf"
     },
     {
-        title: "A Wizard, member of the Istari Order, and leader of the Fellowship of the Ring and The Army of the West:"
+        title: "A Wizard, member of the Istari Order, and leader of the Fellowship of the Ring and The Army of the West:",
         choices: ["Saruman the White", "Legolas Greenleaf", "Gollum", "Gandalf"],
         answer: "Gandalf"
     }
 ];
 
-function startScreen(){}
+var gameScreens = [
+    {
+        id: "startScreen",
+        title: "Lord of the Rings: Name That Character!",
+        content: "Can you guess the character based on the description?",
+        buttons: ["Start!", "High Scores"],
+        buttonFunctions: ["startGame()", "showHighScores()"]
+    },
+    {
+        id: "highScores",
+        title: "High Scores",
+        content: "",
+        buttons: ["Back", "Clear High Scores"],
+        buttonFunctions: [];
+    },
+    {
+        id: "scoreCard",
+        title: "Results!",
+        content: "",
+        buttons: ["Enter Your Score", "Start Over"],
+        buttonFunctions: [];
+    }
+];
 
+var card = document.querySelector("#cardBody");
+var cardTitle = document.querySelector(".card-title");
+var cardText = document.querySelector(".card-text");
+var buttonField = document.querySelector("#buttonField");
+
+//for changing between menu pages, NOT game question pages!
+//right now this is hardcoded to open the startScreen. How can I make it dynamically
+//pick the desired screen?
+function changeCard(id){
+    var gsIndex = 0;
+    for (var i = 0; i < gameScreens.length; i++) {
+        // if the object at index i contains an id that matches the argument passed in, 
+        // gsIndex = i.
+        // then run a function that builds a new page with the index of the object i 
+        // in gameScreens. 
+    }
+
+    // cardTitle.textContent = gameScreens[0].title;
+    // cardText.textContent = gameScreens[0].content;
+    // for (var i = 0; i < gameScreens[0].buttons.length; i++){
+    //     var newButton = document.createElement("a");
+    //     newButton.setAttribute("class", "btn btn-primary");
+    //     newButton.setAttribute("id", "button-" + i);
+    //     newButton.setAttribute("onclick", gameScreens[0].buttonFunctions[i])
+    //     newButton.innerHTML = gameScreens[0].buttons[i];
+    //     buttonField.appendChild(newButton);
+    // }
+}
+
+//
 function startGame(){}
 
 function showHighScores(){}
 
 function nextQuestion(){}
+
+// when the user first arrives at the page,
+// they are greeted with the "start screen"
+    // start screen gets built with the beginGame function, declared onload.
